49e0b08ef4166209bf716ab46e49380ed541b01f
System Administrator <root@chovans.local>
2015-09-13 22:24:16 +0800

    init

---
+++ WaterFallUtil/WaterFallLayout.swift
@@ -0,0 +1,21 @@
+//
+//  WaterFallLayout.swift
+//  WaterFallUtil
+//
+//  Created by chovans on 15/9/13.
+//  Copyright © 2015年 chovans. All rights reserved.
+//
+
+import UIKit
+
+class WaterFallLayout: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
